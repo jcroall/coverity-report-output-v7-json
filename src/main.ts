bfcbab594a0f72cde131e2067e9e87e4735cc8a3
James Croall <jcroall@pobox.com>
2022-03-20 19:39:33 +0000

    Remove common Coverity and GitHub code

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,42 @@
 import fs from 'fs'
-import {createIssueComment, createReview, getExistingIssueComments, getExistingReviewComments, getPullRequestDiff, updateExistingIssueComment, updateExistingReviewComment} from './github/pull-request'
-import {CoverityIssuesView, IssueOccurrence} from './json-v7-schema'
-import {COMMENT_PREFACE, createReviewCommentMessage, createIssueCommentMessage, DiffMap, getDiffMap, createNoLongerPresentMessage, isPresent} from './reporting'
-import {isPullRequest, relativizePath} from './github/github-context'
-import {COVERITY_PASSWORD, COVERITY_PROJECT_NAME, COVERITY_URL, COVERITY_USERNAME, JSON_FILE_PATH} from './inputs'
+import {COVERITY_PASSWORD, COVERITY_PROJECT_NAME, COVERITY_URL, COVERITY_USERNAME, DEBUG_MODE, GITHUB_TOKEN, JSON_FILE_PATH} from './inputs'
 import {info, setFailed, warning} from '@actions/core'
-import {NewReviewComment} from './_namespaces/github'
-import {mapMatchingMergeKeys, ProjectIssue} from './issue-mapper'
+import {COMMENT_PREFACE, coverityCreateIssueCommentMessage, coverityCreateNoLongerPresentMessage, coverityCreateReviewCommentMessage, coverityIsPresent, CoverityIssueOccurrence, CoverityProjectIssue, DiffMap, githubCreateIssueComment, githubCreateReview, githubGetDiffMap, githubGetExistingIssueComments, githubGetExistingReviewComments, githubGetPullRequestDiff, githubIsPullRequest, githubUpdateExistingIssueComment, githubUpdateExistingReviewComment, logger} from '@jcroall/synopsys-sig-node/lib'
+import {coverityMapMatchingMergeKeys} from '@jcroall/synopsys-sig-node/lib/utils/coverity-issue-mapper'
+import {CoverityIssuesView} from '@jcroall/synopsys-sig-node/lib/models/coverity-json-v7-schema'
+import {NewReviewComment} from '@jcroall/synopsys-sig-node/lib/_namespaces/github'
 
 async function run(): Promise<void> {
-  if (!isPullRequest()) {
-    info('Not a Pull Request. Nothing to do...')
+  logger.info('Starting Coverity GitHub Action')
+
+  if (DEBUG_MODE) {
+    logger.level = 'debug'
+    logger.debug(`Enabled debug mode`)
+  }
+
+  if (!githubIsPullRequest()) {
+    logger.info('Not a Pull Request. Nothing to do...')
     return Promise.resolve()
   }
 
-  info(`Using JSON file path: ${JSON_FILE_PATH}`)
+  logger.info(`Using JSON file path: ${JSON_FILE_PATH}`)
 
   // TODO validate file exists and is .json?
   const jsonV7Content = fs.readFileSync(JSON_FILE_PATH)
   const coverityIssues = JSON.parse(jsonV7Content.toString()) as CoverityIssuesView
 
-  let mergeKeyToIssue = new Map<string, ProjectIssue>()
+  let mergeKeyToIssue = new Map<string, CoverityProjectIssue>()
 
   const canCheckCoverity = COVERITY_URL && COVERITY_USERNAME && COVERITY_PASSWORD && COVERITY_PROJECT_NAME
   if (!canCheckCoverity) {
-    warning('Missing Coverity Connect info. Issues will not be checked against the server.')
+    logger.warning('Missing Coverity Connect info. Issues will not be checked against the server.')
   } else {
     const allMergeKeys = coverityIssues.issues.map(issue => issue.mergeKey)
     const allUniqueMergeKeys = new Set<string>(allMergeKeys)
 
     if (canCheckCoverity && coverityIssues && coverityIssues.issues.length > 0) {
       try {
-        mergeKeyToIssue = await mapMatchingMergeKeys(allUniqueMergeKeys)
+        mergeKeyToIssue = await coverityMapMatchingMergeKeys(COVERITY_URL, COVERITY_USERNAME, COVERITY_PASSWORD, COVERITY_PROJECT_NAME, allUniqueMergeKeys)
       } catch (error: any) {
         setFailed(error as string | Error)
         return Promise.reject()
@@ -40,12 +45,12 @@ async function run(): Promise<void> {
   }
 
   const newReviewComments = []
-  const actionReviewComments = await getExistingReviewComments().then(comments => comments.filter(comment => comment.body.includes(COMMENT_PREFACE)))
-  const actionIssueComments = await getExistingIssueComments().then(comments => comments.filter(comment => comment.body?.includes(COMMENT_PREFACE)))
-  const diffMap = await getPullRequestDiff().then(getDiffMap)
+  const actionReviewComments = await githubGetExistingReviewComments(GITHUB_TOKEN).then(comments => comments.filter(comment => comment.body.includes(COMMENT_PREFACE)))
+  const actionIssueComments = await githubGetExistingIssueComments(GITHUB_TOKEN).then(comments => comments.filter(comment => comment.body?.includes(COMMENT_PREFACE)))
+  const diffMap = await githubGetPullRequestDiff(GITHUB_TOKEN).then(githubGetDiffMap)
 
   for (const issue of coverityIssues.issues) {
-    info(`Found Coverity Issue ${issue.mergeKey} at ${issue.mainEventFilePathname}:${issue.mainEventLineNumber}`)
+    logger.info(`Found Coverity Issue ${issue.mergeKey} at ${issue.mainEventFilePathname}:${issue.mainEventLineNumber}`)
 
     const projectIssue = mergeKeyToIssue.get(issue.mergeKey)
     let ignoredOnServer = false
@@ -53,11 +58,11 @@ async function run(): Promise<void> {
     if (projectIssue) {
       ignoredOnServer = projectIssue.action == 'Ignore' || projectIssue.classification in ['False Positive', 'Intentional']
       newOnServer = projectIssue.firstSnapshotId == projectIssue.lastSnapshotId
-      info(`Issue state on server: ignored=${ignoredOnServer}, new=${newOnServer}`)
+      logger.info(`Issue state on server: ignored=${ignoredOnServer}, new=${newOnServer}`)
     }
 
-    const reviewCommentBody = createReviewCommentMessage(issue)
-    const issueCommentBody = createIssueCommentMessage(issue)
+    const reviewCommentBody = coverityCreateReviewCommentMessage(issue)
+    const issueCommentBody = coverityCreateIssueCommentMessage(issue)
 
     const reviewCommentIndex = actionReviewComments.findIndex(comment => comment.line === issue.mainEventLineNumber && comment.body.includes(issue.mergeKey))
     let existingMatchingReviewComment = undefined
@@ -72,51 +77,51 @@ async function run(): Promise<void> {
     }
 
     if (existingMatchingReviewComment !== undefined) {
-      info(`Issue already reported in comment ${existingMatchingReviewComment.id}, updating if necessary...`)
+      logger.info(`Issue already reported in comment ${existingMatchingReviewComment.id}, updating if necessary...`)
       if (existingMatchingReviewComment.body !== reviewCommentBody) {
-        updateExistingReviewComment(existingMatchingReviewComment.id, reviewCommentBody)
+        githubUpdateExistingReviewComment(GITHUB_TOKEN, existingMatchingReviewComment.id, reviewCommentBody)
       }
     } else if (existingMatchingIssueComment !== undefined) {
-      info(`Issue already reported in comment ${existingMatchingIssueComment.id}, updating if necessary...`)
+      logger.info(`Issue already reported in comment ${existingMatchingIssueComment.id}, updating if necessary...`)
       if (existingMatchingIssueComment.body !== issueCommentBody) {
-        updateExistingIssueComment(existingMatchingIssueComment.id, issueCommentBody)
+        githubUpdateExistingIssueComment(GITHUB_TOKEN, existingMatchingIssueComment.id, issueCommentBody)
       }
     } else if (ignoredOnServer) {
-      info('Issue ignored on server, no comment needed.')
+      logger.info('Issue ignored on server, no comment needed.')
     } else if (!newOnServer) {
-      info('Issue already existed on server, no comment needed.')
+      logger.info('Issue already existed on server, no comment needed.')
     } else if (isInDiff(issue, diffMap)) {
-      info('Issue not reported, adding a comment to the review.')
+      logger.info('Issue not reported, adding a comment to the review.')
       newReviewComments.push(createReviewComment(issue, reviewCommentBody))
     } else {
-      info('Issue not reported, adding an issue comment.')
-      createIssueComment(issueCommentBody)
+      logger.info('Issue not reported, adding an issue comment.')
+      githubCreateIssueComment(GITHUB_TOKEN, issueCommentBody)
     }
   }
 
   for (const comment of actionReviewComments) {
-    if (isPresent(comment.body)) {
+    if (coverityIsPresent(comment.body)) {
       info(`Comment ${comment.id} represents a Coverity issue which is no longer present, updating comment to reflect resolution.`)
-      updateExistingReviewComment(comment.id, createNoLongerPresentMessage(comment.body))
+      githubUpdateExistingReviewComment(GITHUB_TOKEN, comment.id, coverityCreateNoLongerPresentMessage(comment.body))
     }
   }
 
   for (const comment of actionIssueComments) {
-    if (comment.body !== undefined && isPresent(comment.body)) {
+    if (comment.body !== undefined && coverityIsPresent(comment.body)) {
       info(`Comment ${comment.id} represents a Coverity issue which is no longer present, updating comment to reflect resolution.`)
-      updateExistingReviewComment(comment.id, createNoLongerPresentMessage(comment.body))
+      githubUpdateExistingReviewComment(GITHUB_TOKEN, comment.id, coverityCreateNoLongerPresentMessage(comment.body))
     }
   }
 
   if (newReviewComments.length > 0) {
     info('Publishing review...')
-    createReview(newReviewComments)
+    githubCreateReview(GITHUB_TOKEN, newReviewComments)
   }
 
   info(`Found ${coverityIssues.issues.length} Coverity issues.`)
 }
 
-function isInDiff(issue: IssueOccurrence, diffMap: DiffMap): boolean {
+function isInDiff(issue: CoverityIssueOccurrence, diffMap: DiffMap): boolean {
   const diffHunks = diffMap.get(issue.mainEventFilePathname)
 
   if (!diffHunks) {
@@ -126,9 +131,9 @@ function isInDiff(issue: IssueOccurrence, diffMap: DiffMap): boolean {
   return diffHunks.filter(hunk => hunk.firstLine <= issue.mainEventLineNumber).some(hunk => issue.mainEventLineNumber <= hunk.lastLine)
 }
 
-function createReviewComment(issue: IssueOccurrence, commentBody: string): NewReviewComment {
+function createReviewComment(issue: CoverityIssueOccurrence, commentBody: string): NewReviewComment {
   return {
-    path: relativizePath(issue.mainEventFilePathname),
+    path: /* TODO: Don't need relatavize? */ /* relativizePath(issue.mainEventFilePathname), */ issue.mainEventFilePathname,
     body: commentBody,
     line: issue.mainEventLineNumber,
     side: 'RIGHT'
